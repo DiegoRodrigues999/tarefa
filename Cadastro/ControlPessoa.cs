493d53dd414583bbc871b1f2684ba4e8a76dee66
DIEGO RODRIGUES DE OLIVEIRA <diego.roliveira18@senacsp.edu.br>
2024-02-06 19:47:44 -0300

    Primeiro Commit

---
+++ Cadastro/ControlPessoa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro
+{
+     class ControlPessoa
+    {
+        public ControlPessoa() 
+        {
+            private readonly string[] _pessoas 
+
+
+            //instanciar uma variavel = Determinar o valor inicial na tela
+            ConsultarOpcao = 0;
+        }//fim do Construtor
+        
+        public int ConsultarOpcao
+         {
+        get
+        {
+            return 0;
+        }
+          
+         }
+
+        public void menu()
+        {
+            Console.WriteLine("Escolha uma das opções abaixo: \n" +
+                              "1. Cadastrar \n +" +
+                              "2. Consultar\n + " +
+                              "3. Atualizar\n +" +
+                              "4. Excluir\n " +
+                              "5. sair");
+        ConsultarOpcao = Convert.to
+        }//fimdo menu
+
+        public void Cadastrar() { }
+    }//fim da classe
+}//fim do projeto
